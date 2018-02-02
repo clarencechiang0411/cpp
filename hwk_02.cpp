68a359a1bd8a24eeb7ec25caf7e13ddc003ce79a
minuetto <clarencechiang0411@users.noreply.github.com>
2018-02-02 10:09:18 +0800

    Update hwk_02.cpp

--- hwk_02.cpp
+++ hwk_02.cpp
@@ -1,1 +1,20 @@
-test
+/* great common divisor*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0;
+    int gcd = 0;
+    
+    cin >> a >> b;
+    for(int i = 1; i <= b && i <= a; i++) {
+        if (a % i == 0 && b % i == 0){
+            gcd = i;
+        }
+    }
+        cout << gcd;
+        return 0;
+}
