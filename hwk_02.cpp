c3c882674109fd7d934ce28381441d348d0eaffc
minuetto <clarencechiang0411@users.noreply.github.com>
2018-02-02 10:59:04 +0800

    Update hwk_02.cpp

--- hwk_02.cpp
+++ hwk_02.cpp
@@ -1,7 +1,6 @@
-/* great common divisor*/
+/*Q3: greatest common divisor*/
 
 #include <iostream>
-
 using namespace std;
 
 int main()
@@ -17,4 +16,31 @@ int main()
     }
         cout << gcd;
         return 0;
+}
+
+
+/*Q4: second greatest common divisor*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0;
+    int gcd = 0;
+    int sgcd = 0;
+    
+    cin >> a >> b;
+    for(int i = 1; i <= b && i <= a; i++) {
+        if (a % i == 0 && b % i == 0){
+            gcd = i;
+        }
+    }
+    
+    for(int j = 1; j <= gcd/2; j++) {
+        if (gcd % j == 0){
+            sgcd = j;
+        }
+    }    
+    cout << sgcd;
+    return 0;
 }
