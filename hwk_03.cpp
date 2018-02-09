c36d0b45def04f15ae590fcea460a62d614e7db0
minuetto <clarencechiang0411@users.noreply.github.com>
2018-02-09 17:57:53 +0800

    Update hwk_03.cpp

--- hwk_03.cpp
+++ hwk_03.cpp
@@ -1,13 +1,32 @@
 /*
 Q3
+!!!!!still on working!!!!!
 */
-
 #include <iostream>
 using namespace std;
-
+ 
 int main() {
-
-
-
-return 0;
+    int sTarget = 0, sMin = 0, cStock = 0, cLack = 0, inv = 0, total = 0;
+    cin >> sTarget >> sMin >> cStock >> cLack >> inv >> total;
+ 
+    int stockTotalCost = 0, lackTotalCost = 0, lackDay = 0;
+ 
+    int daily[total]={0};
+    for(int i = 0; i <= total; i++){
+        cin >> daily[i];
+ 
+        inv -= daily[i];
+        if(inv > 0) {
+            stockTotalCost += inv*cStock;
+        } 
+        else {
+        	lackTotalCost += -1*inv*cLack;
+        	inv = sTarget;
+            lackDay ++;
+        }
+    }
+    cout << lackDay << endl;
+    cout << stockTotalCost+lackTotalCost;
+ 
+	return 0;
 }
